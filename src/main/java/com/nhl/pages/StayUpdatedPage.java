3323d6c212c9c6e39319ba8b09167aa9f70dba68
Shiwantha Lakmal <shiwantha.lakmal@nagarro.com>
2022-07-06 14:48:35 +0400

    added missing class in dev branch stayupdatepage

--- src/main/java/com/nhl/pages/StayUpdatedPage.java
+++ src/main/java/com/nhl/pages/StayUpdatedPage.java
@@ -2,14 +2,13 @@ package com.nhl.pages;
 
 import org.openqa.selenium.By;
 
-import static com.nhl.utils.SeleniumUtils.*;
+import static com.nhl.utils.SeleniumUtils.click;
+import static com.nhl.utils.SeleniumUtils.sendKeys;
 
 public class StayUpdatedPage {
 
     private static final By EMAIL = By.xpath("//*[@class='android.widget.EditText']");
     private static final By CONTINUE = By.xpath("//*[@content-desc='Continue']");
-    public static final By ERROR_MESSAGE = By.xpath("//android.widget.EditText/following-sibling::android.view.View[1]");
-    public static final By SKIP = By.xpath("//android.widget.Button[@content-desc='Skip']");
 
     public StayUpdatedPage(){
     }
@@ -21,18 +20,6 @@ public class StayUpdatedPage {
 
     public void clickContinue(){
         click(CONTINUE, "Continue Button");
-    }
-
-    public boolean checkIfContinueButtonIsEnabled(){
-        return isEnabled(CONTINUE, "Continue Button");
-    }
-
-    public void clickSkip(){
-        click(SKIP, "Skip link");
-    }
-
-    public String getErrorMessage(){
-        return getContentDesc(ERROR_MESSAGE);
     }
 
 }
