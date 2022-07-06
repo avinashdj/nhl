3c28bb62c6ad58a537c60d686833d14226ebe66b
Shiwantha Lakmal <shiwantha.lakmal@nagarro.com>
2022-07-06 09:47:32 +0400

    load top-menu as a composition under welcome-page to reuse common componet

--- src/main/java/com/nhl/pages/WelcomePage.java
+++ src/main/java/com/nhl/pages/WelcomePage.java
@@ -1,5 +1,7 @@
 package com.nhl.pages;
 
+import com.nhl.pages.menunavigations.MenuMainPage;
+import com.nhl.pages.pagecomponent.TopMenuComponent;
 import org.openqa.selenium.By;
 
 import static com.nhl.utils.SeleniumUtils.click;
@@ -9,9 +11,21 @@ public class WelcomePage {
     private static final By LETS_GET_STARTED = By.xpath("(//android.widget.Button[(@content-desc,'Let')])[2]");
 
     public WelcomePage(){
+		this.topMenuComponent = new TopMenuComponent();
     }
 
     public void clickLetsGetStartedButton(){
         click(LETS_GET_STARTED, "Let's Get Started Button");
+    }
+	
+	/**
+     * action : perform top right-conver menu icon
+     * @author shiwantha
+     * @update 5/7/2022
+     * @return
+     */
+    public MenuMainPage switchTOMenu(){
+        this.topMenuComponent.clickOnMenu("Menu");
+        return new MenuMainPage();
     }
 }
